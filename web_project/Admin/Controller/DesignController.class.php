c5660ff08c082f9335014786a33127069b46ed57
Yifei Xu <yifei.xu01@student.adelaide.edu.au>
2021-06-03 18:58:04 +0800

    please see details in update.md

--- web_project/Admin/Controller/DesignController.class.php
+++ web_project/Admin/Controller/DesignController.class.php
@@ -7,14 +7,14 @@
         }
 
         /*
-         *  21 Apr 2021 22:15:54
-         *  back-end managae centre - project list
+         *  2015年3月8日22:15:54
+         *  后台管理中心-毕设列表
          */
         public function index($gpThrId = null, $gpContent = null, $state = null, $gpSHState = null){
             $titles = array();
-            $titles['prt'] = "project of Team05";
+            $titles['prt'] = "projects";
             $titles['prtLink'] = CONTROLLER_NAME;
-            $titles['son'] = "login info";
+            $titles['son'] = "login information";
             $this->assign("titles", $titles);
 
             $where = array();
@@ -46,27 +46,26 @@
             }
             $this->assign("seachData", $seachData);
 
-            $obj    = M('gproject'); // Instantiate the User object
-            $count  = $obj->join("left join teacher on gproject.gpThrId = teacher.thrId")->where($where)->Count();// Query the total number of records meeting the requirements
-            $Page   = new \Think\Page($count,10);// Instantiate the total number of incoming records and the number of records displayed per page of the paging class (10)
-            $show   = $Page->show();//Paging display output//Paging data query Note that the parameters of the limit method should use the attributes of the Page class
+            $obj    = M('gproject'); // 实例化User对象
+            $count  = $obj->join("left join teacher on gproject.gpThrId = teacher.thrId")->where($where)->Count();// 查询满足要求的总记录数
+            $Page   = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+            $show   = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
             $bsList = $obj->join("left join teacher on gproject.gpThrId = teacher.thrId")->field("gproject.*, teacher.thrRealName")->where($where)->limit($Page->firstRow.','.$Page->listRows)->select();
-            var_dump($bsList);die;
-            $this->assign('bsList', $bsList);//assign data set
-            $this->assign('page',$show);//Assign paging output
+            $this->assign('bsList', $bsList);// 赋值数据集
+            $this->assign('page',$show);// 赋值分页输出
 
             $this->display();
         }
 
         /*
-         *  21 Apr 2021 22:16:09
-         *  back-end managae centre - trash
+         *  2015年3月8日22:16:09
+         *  后台管理中心-回收站
          */
         public function recycle(){
             $titles = array();
-            $titles['prt'] = "project of Team05";
+            $titles['prt'] = "projects";
             $titles['prtLink'] = CONTROLLER_NAME;
-            $titles['son'] = "login info";
+            $titles['son'] = "login information";
             $this->assign("titles", $titles);
 
             $obj = M("gproject");
@@ -77,8 +76,8 @@
         }
 
         /*
-         *  21 Apr 2021 17:34:47
-         *  centre projects details
+         *  2015年3月18日17:34:47
+         *  中心课题详情
          */
         public function checkGP($id = 0){
             if($id != 0){
@@ -97,8 +96,8 @@
 
 
         /*
-         *  21 Apr 2021 17:34:59
-         *  centre delete projects
+         *  2015年3月18日17:34:59
+         *  中心删除课题
          */
         public function cycGP($id = 0){
             if($id != 0){
@@ -109,20 +108,20 @@
                 if($flag){
                     $this->success("project delete successful");
                 }else{
-                    $this->error("project fail to delete");
+                    $this->error("project delete fail");
                 }
             }else{
-                $this->error("action failure, please check your actions");
+                $this->error("check you operation and do it again");
             }
         }
 
         /*
-         *  21 Apr 2021 17:42:52
-         *  restore project status
+         *  2015年3月18日17:42:52
+         *  将课题状态恢复
          */
         public function recoverOne($id = 0){
             if($id == 0){
-                $this->error('action failure, please check');
+                $this->error('check you operation and do it again');
             }else{
                 $obj = M('gproject');
                 $where['gpId'] = $id;
@@ -131,49 +130,132 @@
 
                 $flag = $obj->where($where)->save($data);
                 if($flag){
-                    $this->success('restore success, the project has been recovered');
+                    $this->success('recover successful');
                 }else{
-                    $this->error('resrore failure, please check the actions');
+                    $this->error('recover fail');
                 }
             }
         }
 
         /*
-         *  21 Apr 2021 17:42:58
-         *  clear proposal
+         *  2015年3月18日17:42:58
+         *  将课题物理删除
          */
         public function clearOne($id = 0){
             if($id == 0){
-                $this->error('action failure, please check you actions');
+                $this->error('check you operation and do it again');
             }else{
                 $obj = M('gp ');
                 $where['gpId'] = $id;
 
                 $flag = $obj->where($where)->delete();
                 if($flag){
-                    $this->success('clear proposal success');
+                    $this->success('delete successful');
                 }else{
-                    $this->error('clear proposal failure, please check your actions');
+                    $this->error('delete fail');
                 }
             }
         }
 
         /*
-        *   21 Apr 2021 14:56:10
-        *   admin examine proposal
+        *   2015年4月17日14:56:10
+        *   管理员审核课题
         */
         public function SH($id = 0, $flag = 0){
             if($id == 0 || $flag == 0){
-                $this->error('parameter wrong, please check your input');
+                $this->error('data wrong');
             }else{
                 $where['gpId'] = $id;
                 $data['state'] = $flag;
 
                 $obj = M('gproject');
                 if($obj->where($where)->save($data)){
-                    $this->success('examine success');
+                    $this->success('examination passed');
                 }else{
-                    $this->error('examine failure');
+                    $this->error('examination fail');
+                }
+            }
+        }
+
+         /*
+         修改页
+        */
+        public function editGP($id = 0){
+            if($id != 0){
+                $where["gpId"] = $id;
+
+                $obj = M("gproject");
+                $GpDetail = $obj->where($where)->find();
+
+
+                if(is_array($GpDetail) && !empty($GpDetail)){
+                    $this->assign("gpDetail", $GpDetail);
+                    $this->assign("title", "modify proposal");
+                    $this->assign("host",$_SERVER['SERVER_NAME']);
+
+                    $this->display("edit");
+                }else{
+                    $this->error("Operation error, please check");
+                }
+
+            }else{
+                $this->error("Operation error, please check");
+            }
+        }
+
+        /*
+
+         *  提交修改毕设
+         */
+        public function updateGp(){
+            $files = $_FILES['upfile'];
+            if(!empty($files['tmp_name'])){
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     3145728 ;// 设置附件上传大小
+                $upload->exts      =     array('pdf','docx','doc','jpg','png', 'jpeg');// 设置附件上传类型
+                $upload->rootPath  =     'Public'; // 设置附件上传根目录
+                $upload->savePath  =     '/upload/'; // 设置附件上传（子）目录
+                // 上传文件
+                $info   =   $upload->uploadOne($files);
+
+                if(!$info) {// 上传错误提示错误信息
+                    $this->error($upload->getError());
+                }else{
+                    // 上传成功
+                    $infopath = $info['savepath'].$info['savename'];
+                    $data['filePath'] = $infopath;
+                }
+            }
+
+            if(IS_POST){
+                $where['gpId'] = I("post.id");
+
+                $data['gpThrId'] = session('ID');
+                $data['gpTitle'] = I("post.title");
+                $data['gpContent'] = I("post.content");
+                $data['gpAim'] = I("post.aim");
+                $data['gpRequest'] = I("post.request");
+                $data['gpMust'] = I("post.must");
+                $data['gpFormal'] = I("post.formal");
+                $data['gpOthers'] = I("post.others");
+                $data['gpSHState'] = I("post.SHState") == 2 ? 2 : 1;
+                $obj = M("gproject");
+                $admin_state = $obj->where($where)->getField('admin_state');
+                if($admin_state == '-1' || $admin_state == 0){
+                    $data['state'] = 0;
+                }else{
+                    $data['state'] = 1;
+                }
+
+
+                $time = time();
+                $data['updateTime'] = $time;
+
+
+                if($obj->where($where)->save($data)){
+                    $this->success("The subject was successfully modified", U('Design/index'));
+                }else{
+                    $this->error("Subject modification failed, please check");
                 }
             }
         }
---
+++ web_project/update.md
@@ -0,0 +1,8 @@
+03.06.2021 updates by Yifei Xu
+1.	fixed logic problem that after teacher adding a proposal, then admin reject. Go back to teacher account, click modify & submit, the state will become pass without admin approval.
+2.	Adding a button that admin chould change proposal details directly.
+3.	Enabling Admin to upload a file when rejecting a proposal and teacher need to click "Not through" button to view comment and download the file from admin.
+
+03.06.2021 updates by Yifei Xu
+1.	Enabling Admin to view file uploaded by teacher in view window
+2.	Enabling Admin to upload new file through modify window & teacher is able to open new file directly in view window
